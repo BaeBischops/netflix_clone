a797f89afb506c9e44ea89a07ca52e3f0abe9450
BaeBischops <dr.bkzee@gmail.com>
2024-03-20 22:47:52 +0200

    update

--- netflix-ui/src/pages/Login.jsx
+++ netflix-ui/src/pages/Login.jsx
@@ -25,32 +25,32 @@ const Login = () => {
 
   return (
     <Container>
-      <BackgroundImage />
-      <div className="content">
-        <Header />
-        <div className="form-container flex column a-center j-center">
-          <div className="form flex column a-center j-center">
-            <div className="title">
-              <h3>Login</h3>
+        <BackgroundImage />
+        <div className="content">
+            <Header />
+            <div className="form-container flex column a-center j-center">
+                <div className="form flex column a-center j-center">
+                    <div className="title">
+                        <h3>Login</h3>
+                    </div>
+                    <div className="container flex column">
+                        <input
+                        type="text"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        />
+                        <input
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        />
+                        <button onClick={handleLogin}>Login to your account</button>
+                    </div>
+                </div>
             </div>
-            <div className="container flex column">
-              <input
-                type="text"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-              <button onClick={handleLogin}>Login to your account</button>
-            </div>
-          </div>
         </div>
-      </div>
     </Container>
   );
 }
