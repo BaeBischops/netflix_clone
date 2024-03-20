10599f86efff7a27b0032025aad5ff74f8e31066
BaeBischops <dr.bkzee@gmail.com>
2024-03-20 23:01:28 +0200

    update

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Netflix = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    window.onscroll = () => {
+        setIsScrolled(window.screenY === 0? false: true);
+        return () => (window.onscroll = null);
+    };
+
     return (
         <div>
-           NETFLIX 
+           <Navbar isScrolled={isScrolled}/> 
         </div>
     );
 };
