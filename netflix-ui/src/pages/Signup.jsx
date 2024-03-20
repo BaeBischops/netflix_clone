fc63c748177c4954ed8f52612fd6f1b7045e13cb
BaeBischops <dr.bkzee@gmail.com>
2024-03-20 14:39:20 +0200

    update

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -8,6 +8,19 @@ export default function Signup() {
     <Container>
         <BackgroundImage />
         <Header />
+        <div className="body flex column a-center j-center">
+            <div className="text flex column">
+                <h1>Unlimited movies, TV shows, and more</h1>
+                <h4>Watch anywhere. Cancel anytime.</h4>
+                <h6>Ready to watch? Enter your email to create or restart your membership.</h6>
+            </div>
+            <div className="form">
+                <input type="email" name="email" id="email" placeholder="email address" />
+                <input type="password" name="password" id="password" placeholder='password' />
+                <button>Get Started</button>
+            </div>
+            <button>Log In</button>
+        </div>
     </Container>
     );
     
