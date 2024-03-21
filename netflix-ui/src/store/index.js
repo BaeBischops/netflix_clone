6f94d450668fd930f142d2adf8593e19b7772342
BaeBischops <dr.bkzee@gmail.com>
2024-03-21 16:49:07 +0200

    update

---
+++ netflix-ui/src/store/index.js
@@ -0,0 +1,20 @@
+//redux store
+import {configureStore, createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    movies: [],
+    genresLoaded: false,
+    generes: [],
+};
+
+const NetflixSlice = createSlice({
+    name: 'Netflix',
+    initialState,
+    extraReducers: (builder) => {},
+});
+
+export const store = configureStore({
+    reducer: {
+        netflix: NetflixSlice.reducer,
+    },
+});
