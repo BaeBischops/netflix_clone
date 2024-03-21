ecc1369609b8503f2aa568acb1c1eef361ae95f9
BaeBischops <dr.bkzee@gmail.com>
2024-03-22 01:11:51 +0200

    update

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/netflix',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-mongoose.connect();
 
 app.listen(5000, console.log('sever is started!'))
